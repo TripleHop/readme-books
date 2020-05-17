1e94b175f4e8aae22f6c3d1ec8e612dab53b564b
triplehop <jwillcock00@gmail.com>
2020-05-18 09:49:48 +1000

    basic crud added

---
+++ db/migrate/20200515025919_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :book_title
+      t.string :author
+      t.text :blurb
+      t.integer :price
+      t.integer :condition
+
+      t.timestamps
+    end
+  end
+end
